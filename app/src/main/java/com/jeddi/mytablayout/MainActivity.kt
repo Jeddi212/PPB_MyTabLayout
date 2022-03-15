3e105dd0071bd50f51eec6f150c328231659ce89
Jeddi212 <jedediahf51@gmail.com>
2022-03-15 16:20:50 +0700

    Halo Halo Bandung 🏗

---
+++ app/src/main/java/com/jeddi/mytablayout/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.jeddi.mytablayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val sectionPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+
+        var view_pager = findViewById<ViewPager>(R.id.view_pager)
+        view_pager.adapter = sectionPagerAdapter
+
+        var tabs = findViewById<TabLayout>(R.id.tabs)
+        tabs.setupWithViewPager(view_pager)
+
+        supportActionBar?.elevation = 0f
+    }
+}
